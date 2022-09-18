f9d5517958b037f8920f0d3040c74c1abea64e37
孟凡旭 <15733225953@163.com>
2022-09-18 16:40:19 +0800

    第二页

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,7 +5,8 @@
                :fadeOut="true">
     </vue-typed> -->
     <!-- 页面一 -->
-    <div class="page-container page1-container">
+    <div v-if="page === 1"
+         class="page-container page1-container">
       <div class="title">送你的一封信</div>
       <div class="envelope-wrap">
         <img class="envelope"
@@ -17,6 +18,24 @@
       </div>
       <a class="start"
          href="javascript:;">开启</a>
+    </div>
+    <!-- 页面二 -->
+    <div v-if="page === 2"
+         class="page-container page2-container">
+      <div class="title">
+        <p>我们已经相识</p>
+        <p><span>3 </span>个月了！</p>
+      </div>
+      <div class="con">
+        <p>第一天聊天，仿佛昨日</p>
+        <p>
+          <span>7</span>月<span>24</span>日
+        </p>
+        <img src="../assets/img/one.jpeg"
+             alt="">
+      </div>
+      <a class="start"
+         href="javascript:;">开开心心</a>
     </div>
   </div>
 </template>
@@ -43,8 +62,10 @@ export default defineComponent({
     //   console.log(txtArr)
     // }
     // fontAni(originTxt.value)
-
-    return {}
+    const page: Ref<Number> = ref(2)
+    return {
+      page,
+    }
   },
 })
 </script>
